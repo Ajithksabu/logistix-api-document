f01f9fd9cc0a6816cd8473168acecaea81e62609
Ajith K Sabu <ajith@strebentechnik.com>
2022-06-14 13:49:24 +0530

    file upload code added

---
+++ src/main/java/com/strebentechnik/logistix/document/FormData.java
@@ -0,0 +1,21 @@
+package com.strebentechnik.logistix.document;
+
+import java.io.File;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
+
+public class FormData {
+
+    @RestForm("file")
+    public File data;
+
+    @RestForm
+    @PartType(MediaType.TEXT_PLAIN)
+    public String filename;
+
+    @RestForm
+    @PartType(MediaType.TEXT_PLAIN)
+    public String mimetype;
+
+}
--- src/test/java/com/strebentechnik/logistix/document/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.strebentechnik.logistix.document;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
